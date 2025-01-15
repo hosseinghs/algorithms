10e462ff406b6f48ffc553e27d5e51eec336d30b
hosseinghs <job.hghs@gmail.com>
2025-01-15 13:35:45 +0330

    implement queue

---
+++ stacks-queues/queue.js
@@ -0,0 +1,35 @@
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null
+        this.last = null
+        this.size = 0
+    }
+
+    enqueue(val) {
+        const newNode = new Node(val)
+        if (this.size === 0) {
+            this.first = newNode
+            this.last = newNode
+        } else {
+            this.last.next = newNode
+            this.last = newNode
+        }
+        return ++this.size
+    }
+
+    dequeue() {
+        if (this.size === 0) return null
+        if (this.size === 1) {
+            this.first = null
+            this.last = null
+        } else this.first = this.first.next
+        return --this.size
+    }
+}
